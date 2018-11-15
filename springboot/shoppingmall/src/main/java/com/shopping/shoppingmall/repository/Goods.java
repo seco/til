78d121372eb78dbe30729eac90ece15d9fd4a422
Shin, Yun-Cheol <younchul.shin@gmail.com>
2018-11-15 10:14:19 +0900

    repository 수정

--- springboot/shoppingmall/src/main/java/com/shopping/shoppingmall/repository/Goods.java
+++ springboot/shoppingmall/src/main/java/com/shopping/shoppingmall/repository/Goods.java
@@ -4,7 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class Goods extends JpaRepository<Goods, Long> {
+public interface Goods extends JpaRepository<Goods, Long> {
 
     public Page<Goods> findByGoodsNameContaining(String goodsName, Pageable pageable);
 }
